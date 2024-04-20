29f5320216bbe8f6db89008ebe1597407fe95a55
uinx1983 <uinx1983@163.com>
2024-04-20 15:50:24 +0800

    屏蔽了信息显示

--- linux/basic-web-p2.sh
+++ linux/basic-web-p2.sh
@@ -2,6 +2,6 @@
 # title:排错场景2
 # date:2024/04/17
 # author:huanglei
-sudo ufw --force reset
-sudo ufw --force enable
-sudo ufw allow 22/tcp
+sudo ufw --force reset 1>/dev/null
+sudo ufw --force enable 1>/dev/null
+sudo ufw allow 22/tcp 1>/dev/null
